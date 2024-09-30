a505b896b2c6bbe7845f1a733413ee1a3666d168
mansoor567 <mansooralam03122505311@gmail.com>
2024-09-30 22:51:17 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,531 @@
+// const userName = "Mansoor  Alam"
+// const passward = 786786
+// if (userName == "Mansoor  Alam" && passward == 786786){
+//     console.log("login succesfuly")
+// }else {
+//     console.log("login faild")
+// }   let myName = "mansoor"
+//     let myage = 40;
+//     console.log(`my name is: ${myName} and my age is ${myage} and i live in karachi`);
+// }
+// myName()
+// myName()
+// myName()
+// ======================================
+// peractice 2
+// Ef and if Eles statmennts
+// let isRaining = true;
+// if (isRaining) {
+//     console.log("wear a raincot.");
+// } else {
+//     console.log("wear sunglsses");
+// }
+// Else if statements
+// let weather = "Cloudy";
+// if (weather === "raining") {
+//    console.log("wear a raincoat.");
+//   } else if (weather === "Cloudy") {
+//     console.log("wear a light jacket.");
+//   } else {
+//     console.log("wear sunglases.");
+//   }
+//   Conditional Ternary Operator 
+// let  ishungry = true;
+// let  snack = ishungry ? "apple" : "water";
+// console.log(`you should have some  ${snack}.`);
+// ==============*=============================
+// logical or opretor
+// let age = 26;
+// if (age >= 18 && age <= 25){
+//     console.log("hello world");
+// }else{
+//     console.log("hello");
+// }
+// parectice 3
+// let email ="mansoor@gmail.com";
+// let password = 223344;
+// if (email == "mansoor@gmail.com" || password == 223344){
+//     console.log("welcome to facebook");
+// }else{
+//     console.log("login failed")
+// }
+// function (){}
+// peractice 3
+// function sum(){
+//     console.log("mansoor,Alam");
+// }
+// sum()
+//  per=4  
+// function sum (){
+//     console.log("mansoos");
+// }
+// sum ()
+//     console.log("sareem")
+// par=5
+// function sum(para: string){
+//     console.log(para)
+// }
+// sum("wellcome to website our yasie")
+// function solve=========
+// let x: string = "hello";
+// console.log(x. toUpperCase());
+// =============8==========================
+// let myName = "mansoor";
+// let myAge = 40;
+// console.log(`my name is: ${myName}`);
+// aur is tarah se bhi milasakte he
+// let myName = "mansoor";
+// let myAge = 40;
+// console.log(`my name is: ${myName} and my age is ${myAge}`);
+// fuction kese kam karta he opar vale code ko funtion me code karte he
+//  function me code ko bar bar run karne  ke liye use karte he
+// function myName(){
+//  
+// partice 2-----------------
+// function fName(){
+//  console.log("hi mansoor")
+//  console.log("how are you")   
+// }
+// fName()
+// fName()
+// ----------------------------
+// poroblam solve
+// function syntax // Es6 tempales literals 
+// return = perameters
+// function greet(name:string) {
+//     return `welcome , ${name}`;
+// }
+// console.log(greet("Mansoor  Alam")); 
+// jitni dafa chaho functio ko call karo 
+// alag alag naam ss====================
+// function greet(name:string) {
+//     return `welcome , ${name}`;
+// }
+// console.log(greet("Mansoor  Alam"));//istarah bhi hojata he
+// console.log("sareem");//istarah se bhi code karsakte he
+//================================
+// function greet(name:string) {
+//         return `welcome , ${name}`;
+//     }
+//     console.log(greet("Mansoor  Alam"));
+// function greet(name:string) {
+//     return `welcome , ${name}`;
+// }
+// console.log(greet("Mansoor  Alam"));
+// function myFun(){
+//     let total=2 + 6;
+//     return total; 
+// } 
+// let data = myFun(); //siryal numbar 166 ka data 170 me ake save hota he fir mujhe  vaha se data farhaam karta he
+// console.log(data);
+//-----------------------------------
+// function myfun(){
+//     let total = 2 + 9; 
+//     return total;
+// }
+// let data = myfun();
+// console.log(data);
+//---------------------------------------------
+// function halffaryEgg():number {
+//     let cocked = 1 + 1.5 + 2;
+//     return cocked
+// }
+// let response: number = halffaryEgg()
+//     console.log(response);
+//-----------------------------------------------
+// function edi():number {
+//     let many = 30 + 70 + 60;
+//     return many
+// }
+// let response: number = edi()
+// console.log(response);
+//----------------------------------
+// function biryani() : Number{
+//     return  2 + 6 + 1.5;
+// }
+// let response = biryani();
+// console.log(response)      // ok he ye
+//-----------------------------------------------------------------
+//FUNCTION(){}     //--------=>PERAMETOR()
+// function add(x:number,y:number) {   //-------perametir
+//     return x + y;
+// }
+// let result = add(5,9);  //----------argument
+// console.log(result);
+//----------------------------------------------
+// function inn(x: number,y: number) {
+//     return x * y;
+// }
+// let result = inn(5,8);
+// console.log(result)
+// ----------------------------------------
+// function, perametor solve
+// Mr Malik Result//work
+// phyics =  40
+// math =  50
+// Chemistry =  60
+// Urdu = 45
+// function 
+// niche vale istarah se bhi code hota he
+// function file(num1:number,num2:number){
+//     console.log(num1*num2);
+// }
+// file(5,4);
+// function (){} sove slesiton========
+// function halffryEgg(){
+//     let cocked = 2 + 1.5 + 2;
+//     console.log(cocked);
+// }
+// halffryEgg()
+// perectice ============
+// function halffryEgg(){
+//     let cocked = 2 + 1.5 + 2;
+//     console.log(cocked)
+// }
+// halffryEgg()
+//------------------return values---------------------
+// function myFriends(){
+//     let bounds = 200 + 750 + 1500;
+//     let many = 600 + 400 + 800;
+//     console.log(many)
+//     console.log(bounds);
+// }
+// myFriends()
+//-----------------------------------
+// function add(){
+//     let frinds = 4+8+9;
+//     console.log(frinds);   
+// }
+// add()
+// peractis ===========
+// let students = ["afeerah","sarim","fabeeha",];
+// console.log(students);
+// doosra tariqa perectise===
+// address           1         2         3 
+// index             0         1         2
+// let students = ["afeerah","sarim","fabeeha",];
+// console.log(students[2]);
+// problam 1: finding a name
+// oroblam 2: finding total elements in list
+// problam 3: deleting items
+// length==================
+// poroblam 2 : frinds a name
+// let studentsname = ["afeerah","sarim","fabeeha"];
+// console.log(studentsname.length);
+// -------------------------------------------
+// peractic
+// let students = ["osama","zaid","hina","ali"];
+// console.log(students.length-1);
+//------------------------------------------------------
+// let students = ["ali","sana","sareem","danish"];
+// console.log(students.length-2);
+//-------------------------------------------------------
+//    Push methed-----------
+// let arr:string[] =  ["ali","sana","sareem","danish"];
+// console.log(arr);
+// arr.push("samreen")
+// console.log(arr)
+//-------------------------------------------------------
+// let arr:string[]=["afeerah","sareem","fabiha"];
+// console.log(arr)
+// arr.push("bazil")
+// console.log(arr)
+// problam 3 deleting itams
+// let names = ["afeerah","sarim","fabeeha","aroosh"];
+// console.log(names.slice(2));
+// console.log(names);
+// problam solve
+// let names = ["afeerah","sarim","fabeeha","aroosh"];
+// console.log(names.splice(2,3));
+// console.log(names);
+// 
+// Switch Statments=======
+// let dayOff = "Sunday";
+// switch (dayOff) {
+//     case "Saturday":
+//     console.log("Go hiking.");
+//     break;
+//     case "Sunday":
+//         console.log("Read a book");
+//     break;
+//     default:
+//         console.log("work on a hobby.");
+// function perctic
+// let students = ["afeerah","fabeeha","sarim"];
+// console.log(students);
+// -------------------------
+// function calculateArea(width: number, height: number): number {
+//     return width * height;
+// }
+// let response: number = calculateArea(100,50)
+// console.log(response);
+//------------------------------------------------------------------
+// Array ki Difanation
+// let person = {
+//     name    :    "sareem",
+//     age     :     30,
+//     cnic    :     123123,
+//}
+// let a = Object.keys(person);
+// console.log(a);
+// let person = {
+//     name    :    "sareem",
+//     age     :     30,
+//     cnic    :     123123,  
+// }
+// let a = Object.valuse(person);
+// console.log(a);
+// let a:any = person.age;
+// let a:any = person.name;
+// console.log(a);
+// console.log(person);
+// Array ki Difanation
+// // Object entries =>es mathad se solve karna he Object (valuse) ko
+// Object key valuse  pairametor
+// Spread Opreater (...)
+// ============+++++===================================
+// let famous_person: string = "Albert Einstein";
+// let message: string = `${famous_person} once said, “A person who never made a mistake never tried anything new.”`;
+// console.log(message);
+//--------------------------------------------------------------------------------------------------------
+// let favoriteNumber: number = 8; // This saves your favorite number
+// console.log(`My favorite number is ${favoriteNumber}.`); // This shows the message with your favorite number
+//------------------------------------------------------------------------
+// let names: string[] = ["Alice", "Bob", "Charlie"];
+// for (let i = 0; i <names.length; i++) {
+//     console.log(names[i]); 
+// };
+//---------------------------------------------------------
+// let names1: string[] = ["ali","nabeel","misbah","haleema"]
+// for(let i = 0; i <= 4; i++){
+//     console.log(names[i]);
+// }
+//----------------------------------------------------------------------------------------
+// let number: number [] = [1,2,3,4,5,6,7,8,9,];
+// for (let i = 0; i < number . length;  i++) {
+//     const numbers = number[i];
+//     let ordinalEnding: string = "th";
+//     if (numbers === 1) {
+//         ordinalEnding = "th";
+//     } else if (numbers === 2) {
+//         ordinalEnding = "2d";
+//     } else (numbers === 3); {
+//         ordinalEnding = "rd";
+//     }
+//     console.log(numbers + ordinalEnding);
+// }
+//-----------------------------------------------------
+//**** FOOR  LOOP ****
+//  let  array: string [] = [
+//     "Apple",
+//     "Mango",
+//     "Orange",
+//     "Bana",
+//     "Grapes",
+//     "Watermelon",
+//     "Melon",
+//     "Kiwi",
+//     "Coconut",
+//     "Pinapple",
+//  ];
+//  array.reverse() 
+//  for (let i= 0; i <array.length; i++) {console.log(`${array[i]}`)
+// };
+//-----------------------------------------------------------------
+//  let freinds=["ali","sana","iqra","shan","aman"]
+//  console.log(`${freinds[2]}is my enemy`)
+//-------------------------------------------------------------------
+// let  array: string [] = [
+//    "Apple",
+//    "Mango",
+//    "Orange",
+//    "Bana",
+//    "Grapes",
+//    "Watermelon",
+//    "Melon",
+//    "Kiwi",
+//    "Coconut",
+//    "Pinapple",
+// ];
+// array.reverse()
+// console.log(`${array[3]} is my enemy`)
+//-------------------------------------------------------
+// *************ENEMY****************
+// let array: string [] = ["ali","osama","zaid","sany","kamal"];
+// for(let i = 0; i <array.length; i++) {
+//    if (array[i] == "kamal") {
+//       console.log(`${array[i]}*******enemy********`);
+//    }else{
+//       console.log(`${array[i]}`);
+//    }
+// }
+//-----------------------------------------------------
+///////PUSH METHED/////////////  aoni taraf se me banay
+// Push methed-----------
+//--------------------------------------------------
+// let arr:string[] =  ["ali","sana","sareem","danish"];
+// console.log(arr);
+// arr.push("samreen")
+// console.log(arr)
+//==================================================
+// let person: {name: string; age: number; address: String;
+//     name: "Rabia Siddique",
+//     age: 30,
+//     age
+// }
+//----------------------------------------------
+//let numbers: number[] = [1, 2, 3, 4, 5];
+// console.log(numbers[0]); // Outputs 1
+// numbers[1] = 10;
+// console.log(numbers); // Outputs [1, 10, 3, 4, 5]
+//------------------------------------------------------ 
+// FOR  LOOP=========
+//task  1: if we want to pass adress here in line  4.how will we define  its typs .and want a nested Oject how can we define typs
+// interface Address {
+//     area: string;
+//     city:string;
+//     country: string;
+// }
+// interface person {
+//    name:string;
+//    age: number;
+//    isStudent?: boolean;
+//    address?: Address;    // Optinol address  property
+// }
+// const person1: person = {
+//     name : "sareem",
+//     age : 30,
+//     isStudent?: true,
+//     address?: {
+//         area: "orangi town",
+//         city: "karachi",
+//         country: "pakistan",
+//     }
+// }
+// console.log('person1',person1)
+//----------------------------------------------
+// let ishungry = true;
+// let snack = ishungry ? "apple" : "water";
+// console.log(`yuo should have some ${snack}.`);
+//-------------------------------------------------------
+// Terrnary Opertor  [ENEMY]
+// let freinds = ["Ahmad","saleem","sareem","osama","Asad","Samreen","Hena","Bilal","Aqsa","Noor","Dilawar"];
+// console.log(`${freinds [1]} is my enemy`);
+//-----------------------------------------------------------------------------------------------------------
+// let freinds = ["Ahmad","saleem","sareem","osama","Asad","Samreen","Hena","Bilal","Aqsa","Noor","Dilawar"];
+// console.log(`${freinds [6]} is my enemy`);
+//---------------------------------------------------------------------------------------------------------------
+// let array: string[]=["Shan","Osama","Ali","Jawed","Akram","Noor","Sana","Aysha","Farheen"];
+// for (let i = 0 ; 1 <array.length; i++){
+//     if (array[i]=="Jawed"){
+//         console.log(`${array[i]}*****enemy*****`);
+//     }else{
+//         console.log(`${array[i]}`);
+//     }
+// };
+//--------------------------------------------------------
+// ANONMOUS  FUNCTION 
+// HOISTING
+// console.log(a)
+// const a = 10;
+// console.log(a)
+//----------CLOSURE  FUNTCTION-----------------
+// function Fun1 (){
+//     let a = 5;
+// console.log(a);
+// }
+// Fun1();
+// //------------------------------------------------
+//PROMISE  FUNCTION
+// PENDING
+// FULFIL
+// REJECT
+//RESOLVe
+// const ticket = new Promise(function (resolve, reject) {
+//     const isBoarded = false;
+//     if(isBoarded) {
+//         resolve("you are not in tha flight");
+//     }else{
+//         reject("your flight has been cancalled");
+//     }
+// });
+// ticket 
+// .then((data) => {
+//     console.log("wohoo",data);
+// })
+// .catch((data) => {
+//     console.log("oh no", data);
+// });
+//---------------------------------------------------------------------------
+//-----------------------------------------------
+// const array = [0, 1, 2, 3, 4, 5];
+// const myObj = Object.groupBy(array, (num, index) => {
+//     return num % 2 === 0 ? "even": "odd";
+// });
+//--------------------------------------------------
+// setTimeout(function () {
+// console.log("hello , world");
+// },3000);
+// Do while loop//
+// let i=1;
+// while(i<=1) {
+//     let s:string = "*";
+//     let j=1;
+//     while (j<=5) {
+//         console.log(s);
+//         s+="*" ;
+//         j++;
+//     }
+// i++;
+// }
+// //------------------------------------------------------------
+// let arr1 = ["1","2","3"];
+// arr1. reverse();
+// console.log(arr1)
+// let arr = ["1","2","3"];
+// arr.reverse();
+// console.log(arr);
+//--------------------------------------------------
+// function processuserInput(callback:(
+//   name: string) =>void) {
+//     let name = "sir ameen alam";
+//     callback(name);
+//   }
+// p
+// rocessuserInput(function(name:string) {
+//     console.log("Hello,"+ name);
+// });
+// -------------------------------------------------
+console.log("start");
+var userName = "Mobashir Ali";
+console.log(userName);
+var age = 18;
+if (age < 18) {
+    console.log("You are not an adult");
+}
+else {
+    console.log("You are an adult");
+}
+var bestFriends = ["Raza", "Ali", "Junaid", "Hamza"];
+for (var i = 0; i < bestFriends.length; i++) {
+    console.log("".concat(bestFriends[i], " is my bestFriend!"));
+}
+var car = {
+    brand: "Toyota",
+    model: "Corolla",
+    isUsed: false,
+    Year: 2012,
+};
+console.log(car["brand"]);
+console.log(car.model);
+console.log("The End");
+//--------------------------------------------------------
+// ENUN  DIRECTION
+// enum  DIRECTION2{
+//     Up="up",
+//     Down="123",
+//     Right="karachi",
+//     Left="India",
+// }
+// console.log(DIRECTION2)
+//--------------------------------
